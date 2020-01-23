0e22b81c179cddbcc7915b01ba3989a495262c7f
Von Matthew Alfafara <valfafara@yondu.com>
2020-01-24 01:18:00 +0800

    Added carousel first item (mobile)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  carouselItem = 1
+  items = []
+
   constructor() { }
 
   ngOnInit() {
+    this.items[0] = true
+    this.items[1] = false
+    this.items[2] = false
+  }
+
+  change(item) {
+    this.items = this.items.fill(false)
+
+    this.items[item] = true
+
+    console.log(this.items)
   }
 
 }
